85d92ecc7aea5a0ddb2b0f387b6e2e0d8398e1d1
QuintusGu <guheimdall@gmail.com>
2018-10-31 22:21:31 +1100

    oct31

--- practise/src/Oct31st2018.java
+++ practise/src/Oct31st2018.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Oct31st2018 {
     /*public static void main(String[] args) {
         int[]a = new int[]{1,2,3};
@@ -6,5 +8,43 @@ public class Oct31st2018 {
         System.out.println(b[1]+b[2]+b[0]);
         System.out.println(a);
     }*/
-    
-}
+    /*public static String numberToString(int num) {
+        return Integer.toString(num);
+    }*/
+    /*public static void main(String[] args) {
+    String name;
+    int age;
+    double weight, income;
+    Scanner keyboard = new Scanner(System.in);
+        System.out.println("name");
+        name = keyboard.next();
+        System.out.println("age");
+        age = keyboard.nextInt();
+        System.out.println("weight");
+        weight = keyboard.nextDouble();
+        System.out.println("income");
+        income = keyboard.nextDouble();
+        System.out.println("so "+name+" "+age+" old and "+weight+" kg and "+income+" salary");
+    }*/
+/*    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        double price,salesTax, total;
+        System.out.print("how much");
+        price = keyboard.nextDouble();
+        salesTax = price*0.0825;
+        total = price+salesTax;
+        System.out.println("the price is\t"+price);
+        System.out.println("the Tax is\t"+salesTax);
+        System.out.println("total\t"+total);
+    }
+}*/
+   /* public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 3 == 0|| i % 5 == 0)
+                continue;
+            System.out.println(i);
+            }
+        }*/
+    }
+
+
