9eb8ccaacda7ec765212ba318861c63f4af7c3fc
Siqi Gu <u6359309@anu.edu.au>
2018-11-10 23:29:04 +1100

    Nov10th2018

--- practise/src/Hero.java
+++ practise/src/Hero.java
@@ -1,24 +1,26 @@
 public class Hero {
-    String name;
-    float hp;
-    float armor;
-    int moveSpeed;
-public Hero(String heroname){
-    name = heroname;
-}
-public Hero(String heroname,float heroHp){
-    name = heroname;
-    hp = heroHp;
-}
-public Hero(String heroname,float heroHp,float heroArmor,int heroMoveSpeed){
-    name = heroname;
-    hp = heroHp;
-    armor = heroArmor;
-    moveSpeed = heroMoveSpeed;
+    public String name;
+    protected float hp;
+public boolean equals(Object o ){
+    if (o instanceof  Hero){
+        Hero h = (Hero)o;
+        return this.hp == h.hp;
+    }
+    return false;
 }
     public static void main(String[] args) {
-        Hero garen = new Hero("盖伦");
-        Hero teemo = new Hero("提莫",350);
-        Hero J4 = new Hero("嘉文四世",400,79,355);
+        Hero h1 = new Hero();
+        h1.hp = 300;
+        Hero h2 = new Hero();
+        h2.hp = 400;
+        Hero h3 = new Hero();
+        h3.hp = 300;
+        System.out.println(h1.equals(h2));
+        System.out.println(h1.equals(h3));
     }
+
+
+
+
+
 }
--- practise/src/HeroType.java
+++
@@ -1,3 +0,0 @@
-public enum HeroType {
-    TANK, WIZARD, ASSASSIN, ASSIST, WARRIOR, RANGED, PUSH, FARMING
-}
