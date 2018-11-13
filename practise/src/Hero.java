b798378ed7a1b7a36cd197bbe006c5eb45c1a53a
QuintusGu <guheimdall@gmail.com>
2018-11-14 00:14:11 +1100

    Nov13rd2018

--- practise/src/Hero.java
+++ practise/src/Hero.java
@@ -1,26 +1,12 @@
 public class Hero {
     public String name;
-    protected float hp;
-public boolean equals(Object o ){
-    if (o instanceof  Hero){
-        Hero h = (Hero)o;
-        return this.hp == h.hp;
+    public float hp;
+    public int damage;
+    public Hero(){}
+    public  Hero(String name){
+        this.name = name;
     }
-    return false;
-}
-    public static void main(String[] args) {
-        Hero h1 = new Hero();
-        h1.hp = 300;
-        Hero h2 = new Hero();
-        h2.hp = 400;
-        Hero h3 = new Hero();
-        h3.hp = 300;
-        System.out.println(h1.equals(h2));
-        System.out.println(h1.equals(h3));
+    public String toString(){
+        return name;
     }
-
-
-
-
-
 }
---
+++ practise/src/Nov13rd2018.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Nov13rd2018 extends Hero {
+ /*   public static void main(String[] args) {
+
+
+        Scanner s = new Scanner(System.in);
+
+        // Get L and R from the input
+        int L = s.nextInt();
+        int R = s.nextInt();
+        for(int i=L;i<=R;i++){
+            System.out.print(i+" ");
+
+        }
+        System.out.println();
+        // Write here the logic to print all integers between L and R
+        s.close();
+    }*/
+/* public static void main(String[] args) {
+     ArrayList heros = new ArrayList();
+     heros.add(new Hero("盖伦"));
+     System.out.println(heros.size());
+     heros.add(new Hero("提莫"));
+     System.out.println(heros.size());
+ }*/
+ public static void main(String[] args) {
+     ArrayList heroes = new ArrayList();
+     for (int i =0;i<5;i++){
+         heroes.add(new Hero("hero "+i));
+     }
+     Hero SpecialHero = new Hero("Special Hero");
+     heroes.add(SpecialHero);
+     System.out.println(heroes);
+     System.out.println(heroes.contains(new Hero("hero 1")));
+     System.out.println(heroes.contains(SpecialHero));
+     System.out.println(heroes.get(5));
+
+ }
+}
