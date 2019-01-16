448e9febf99888f2582343046d5740a4e2172c45
sqg <sqglife@gmail.com>
2019-01-16 17:31:18 +1100

    Jan16th2018-001

---
+++ practise/src/character/GiantDragon.java
@@ -0,0 +1,11 @@
+package character;
+
+public class GiantDragon {
+    private GiantDragon(){
+
+    }
+    private static GiantDragon instance = new GiantDragon();
+    public static GiantDragon getInstance(){
+        return instance;
+    }
+}
--- practise/src/character/Hero.java
+++ practise/src/character/Hero.java
@@ -1,19 +1,27 @@
 package character;
 
 public class Hero {
-    public String name;
-    protected float hp;
-    static String copyRight;
+    public String name = "some hero";
+
+    {
+        System.out.println(name);
+    }
+
+    public Hero() {
+        name = "two hero";
+        System.out.println(name);
+    }
+
+    {
+        name = "one hero";
+        System.out.println(name);
+    }
 
     public static void main(String[] args) {
-        Hero garen = new Hero();
-        garen.name = "garen";
-        Hero.copyRight = "riot";
-        System.out.println(garen.name);
-        System.out.println(garen.copyRight);
+
+
         Hero teemo = new Hero();
-        teemo.name = "teemo";
         System.out.println(teemo.name);
-        System.out.println(teemo.copyRight);
     }
+
 }
