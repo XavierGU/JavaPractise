36150d475168f71384a743c9441c69a0a980930c
SQG <guheimdall@gmail.com>
2019-01-24 20:53:41 +0800

    Jan24th2019-001

--- practise/src/digit/TestNumber.java
+++ practise/src/digit/TestNumber.java
@@ -2,21 +2,11 @@ package digit;
 
 public class TestNumber {
     public static void main(String[] args) {
-        int i =5;
-        byte b = 1;
-        Byte b1 = b;
-        byte b2 = b1;
-        short s = 1;
-        Short s1  = s;
-        short s2 = s1;
-        float f = 0;
-        Float f1 = f;
-        float f2 = f1;
-        double d = 1.0;
-        Double d1 = d;
-        double d2 = d;
-        int i2 = b1;
-        System.out.println(b1.MAX_VALUE);
-        System.out.println(b1.MIN_VALUE);
+        float f = 3.14f;
+        String str = String.valueOf(f);
+        String str2 = "3.1a4";
+        Float f1 = Float.parseFloat(str2);
+        System.out.println(f1);
+
     }
 }
