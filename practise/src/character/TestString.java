5786ea6fcf8bd69744eff8a294a72fa7748788a3
SQG <guheimdall@gmail.com>
2019-01-30 00:27:28 +0800

    Jan29th-bug fixed

--- practise/src/character/TestString.java
+++ practise/src/character/TestString.java
@@ -3,47 +3,49 @@ package character;
 import java.util.Arrays;
 
 public class TestString {
+
     public static void main(String[] args) {
-        String[]ss = new String[8];
-        for (int i = 0;i<ss.length;i++){
+        String[]str= new String[8];
+        for (int i = 0;i<str.length;i++){
             String randomString = randomString(5);
-            ss[i]= randomString;
+            str[i]= randomString;
         }
-        System.out.println("未排序");
-        System.out.println(Arrays.toString(ss));
-        for (int j = 0;j<ss.length;j++){
-            for (int k = 0;k<ss.length-j-1;k++){
-                char firstChar1 = ss[k].charAt(0);
-                char firstChar2 = ss[k+1].charAt(0);
-                firstChar1 = Character.toLowerCase(firstChar1);
-                firstChar2 = Character.toLowerCase(firstChar2);
-                if (firstChar1>firstChar2) {
-                    String temp = ss[k];
-                    ss[k] = ss[k + 1];
-                    ss[k + 1] = temp;
+        System.out.println("排序前");
+        System.out.println(Arrays.toString(str));
+        for (int i = 0; i<str.length;i++){
+            for (int j = 0;j<str.length-i-1;j++){
+                char firstCase1 = str[j].charAt(0);
+                char firstCase2 = str[j+1].charAt(0);
+                firstCase1 = Character.toLowerCase(firstCase1);
+                firstCase2 = Character.toLowerCase(firstCase2);
+                if (firstCase1>firstCase2){
+                    String temp = str[j];
+                    str[j]= str[j+1];
+                    str[j+1] = temp;
                 }
             }
         }
-        System.out.println("已排序");
-        System.out.println(Arrays.toString(ss));
+        System.out.println("排序后");
+        System.out.println(Arrays.toString(str));
     }
     private static String randomString (int length){
         String pool = "";
         for (short i = '0';i<='9';i++){
+            pool+=(char)i;
+        }
+        for (short i = 'a'; i<='z';i++){
             pool += (char)i;
         }
-        for (short i = 'a';i<='z';i++){
-            pool+=(char)i;
-        }
         for (short i = 'A';i<='Z';i++){
-            pool+=(char)i;
+            pool += (char)i;
         }
-        char[]cc = new char[length];
-        for (int i=0;i<cc.length;i++){
+        char[]ca = new char[length];
+        for (int i= 0; i < ca.length;i++){
             int index = (int)(Math.random()*pool.length());
-            cc[i]=pool.charAt(index);
+            ca[i]=pool.charAt(index);
         }
-        String str = new String(cc);
-        return str;
+        String result = new String(ca);
+        return result;
     }
+
 }
