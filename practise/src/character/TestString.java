31e4d655d32e347fbd66c52a77c84b7c650dd624
SQG <guheimdall@gmail.com>
2019-02-03 01:15:51 +0800

    Feb3rd2019

--- practise/src/character/TestString.java
+++ practise/src/character/TestString.java
@@ -2,52 +2,69 @@ package character;
 
 public class TestString {
     public static void main(String[] args) {
-        String password = randomString(3);
-        System.out.println("the password: "+password);
-        char[]guessWord = new char[password.length()];
-        generatePassword(guessWord,password);
-
-    }
-    private static String randomString(int length) {
-        String pool = "";
-        for (short i = '0'; i <= '9'; i++) {
-            pool += (char) i;
-        }
-        for (short i = 'a'; i <= 'z'; i++) {
-            pool += (char) i;
-        }
-        for (short i = 'A'; i <= 'Z'; i++) {
-            pool += (char) i;
-        }
-        char[] cc = new char[length];
-        for (int i = 0; i < cc.length; i++) {
-            int index = (int) (Math.random() * pool.length());
-            cc[i]=pool.charAt(index);
-        }
-        String result = new String(cc);
-        return result;
-    }
-    public static void generatePassword(char[]guessWord, String password){
-        generatePassword(guessWord,0,password);
-    }
-    public static void generatePassword(char[]guessWord, int index, String password){
-        for (short i ='0';i<='Z';i++){
-            char c = (char)i;
-            if (!Character.isLetterOrDigit(c))
-            continue;
-            guessWord[index] = c;
-            if (index!=guessWord.length-1){
-                generatePassword(guessWord,index+1,password);
-            }else{
-                String guess = new String(guessWord);
-                if (guess.equals(password)){
-                    System.out.println("guess: "+  guess);
-                    return;
-                }
+        // String sentence = "盖伦,在进行了连续8次击杀后,获得了超神 的称号";
+        /*char c = sentence.charAt(0);
+        System.out.println(c);
+        String subString1 = sentence.substring(3);
+        System.out.println(subString1);
+        String subString2 = sentence.substring(3,5);
+        System.out.println(subString2);*/
+        /*String subSentences[]= sentence.split(",");
+        for (String sub : subSentences){
+            System.out.println(sub);
+    }*/
+        //System.out.println(sentence.trim());
+       /* System.out.println(sentence.toLowerCase());
+        System.out.println(sentence.toUpperCase());
+*/
+        /*System.out.println(sentence.indexOf('8'));
+        System.out.println(sentence.indexOf("超神"));
+        System.out.println(sentence.lastIndexOf("了"));
+        System.out.println(sentence.indexOf(',',5));
+        System.out.println(sentence.contains("击杀"));*/
+        /*String temp = sentence.replaceAll("击杀","被击杀");
+        System.out.println(temp);
+        temp = temp.replaceAll("超神","超鬼");
+        System.out.println(temp);
+        temp = temp.replaceFirst(",","");
+        System.out.println(temp);*/
+      /*  String sentence =  "peter piper picked a peck of pickled peppers";
+       // System.out.println(sentence.toUpperCase());
+        System.out.println(sentence);
+        String[]words = sentence.split(" ");
+        int count = 0;
+        for (int i = 0;i<words.length;i++) {
+            String word = words[i];
+            char firstWord = word.charAt(0);
+            if (firstWord == 'p') {
+                count++;
             }
-
         }
-
+        System.out.printf("一共有%d个P",count);*/
+      /*cs[cs.length-1]=Character.toUpperCase(cs[cs.length-1]);
+        String result = new String(cs);
+        System.out.println(result);*/
+       /* String s = "lengendary";
+        char[]cs = s.toCharArray();
+        for (int i = 0;i<cs.length;i+=2) {
+            if (i % 2 == 0) {
+                cs[i] = Character.toUpperCase(cs[i]);
+            }
+        }
+      String result = new String(cs);
+        System.out.println(result);
+        }*/
+    String sentence = " has given us that two ears, two eyes, and but one tongue, to the end that we should hear and see more than we speak";
+    int lastTwo = sentence.lastIndexOf("two");
+        System.out.println(lastTwo);
+    char[]cs = sentence.toCharArray();
+    for (int i = 0;i<cs.length;i++){
+        cs[lastTwo]=Character.toUpperCase(cs[lastTwo]);
     }
-}
+    String result = new String(cs);
+        System.out.println(result);
+    }
+    }
+
+
 
