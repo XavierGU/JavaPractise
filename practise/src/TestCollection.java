552354afdadcde4762a4b3c5429f5f300766696d
顾斯琪 <guheimdall@gmail.com>
2019-03-19 00:33:08 +1100

    Mar18th2019-002

--- practise/src/TestCollection.java
+++ practise/src/TestCollection.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
 import charactor.Hero;
 public class TestCollection {
-    public static void main(String[] args) {
+ /*   public static void main(String[] args) {
         ArrayList heros = new ArrayList();
         heros.add(new Hero("gaven"));
         System.out.println(heros.size());
         heros.add(new Hero("timo"));
         System.out.println(heros.size());
-    }
+    }*/
+ public static void main(String[] args) {
+     ArrayList heros = new ArrayList();
+     for (int i = 0;i<5;i++){
+         heros.add(new Hero("hero"+i));
+     }
+     System.out.println(heros);
+     Hero specialHero = new Hero("special hero");
+     heros.add(3,specialHero);
+     System.out.println(heros);
+ }
 }
